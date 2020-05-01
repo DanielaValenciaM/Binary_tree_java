c4605e4cf0487a12b44f96ca7642ceec1431c225
DanielaValenciaM <64657412+DanielaValenciaM@users.noreply.github.com>
2020-05-01 14:47:13 -0500

    Create VEDArbin2.java

---
+++ Arboles/src/VEDArbin2.java
@@ -0,0 +1,26 @@
+
+public class Semana5 {
+
+
+    public static void main(String[] args) {
+        
+        VEDArbin arbolPrueba = new VEDArbin();
+        arbolPrueba.insertar(4);
+        arbolPrueba.insertar(5);
+        arbolPrueba.insertar(2);
+        arbolPrueba.insertar(7);
+        arbolPrueba.insertar(3);
+        arbolPrueba.insertar(1);
+        arbolPrueba.insertar(9);
+        arbolPrueba.insertar(10);
+        arbolPrueba.insertar(8);
+        
+        
+        System.out.println("====================Verificacion funcionamiento metodo=================");
+        System.out.println("Previamente se tiene un arbol binario cargado con los siguientes datos: 1,2,3,4,5,7,8,9,10");
+        System.out.println("Incialmente se verifica existe el valor '6', lo cual es: "+ arbolPrueba.existe(6));
+        System.out.println("Seguidamente se verifica existe el valor '10', lo cual es: "+ arbolPrueba.existe(10));
+        System.out.println("Asi se comprueba que el metodo de busqueda funciona correctamente");
+    }
+    
+}
